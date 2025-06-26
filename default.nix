d13bd13541162ebc6fbbb3aa27a1b0671e11d68f
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2025-06-26 20:31:03 +0630

    feat: update the way install the font

--- default.nix
+++ default.nix
@@ -1,14 +1,24 @@
-{ lib, stdenv }:
+{
+  lib,
+  stdenv,
+  pkgs,
+}:
 
 stdenv.mkDerivation {
   pname = "jetbrains-mono-condensed";
   version = "1.0";
 
-  src = ./.;
+  src = pkgs.fetchFromGitHub {
+    owner = "Oungseik";
+    repo = "JetBrainsMono-Condensed";
+    rev = "68bc38b1d4c1035589996d7c725d988dbaa667de";
+    sha256 = "1b5k56qa9bw3fid3zynjm6yi9gd0sm9xz1zdms50bb86ri8c8jjb";
+  };
 
   installPhase = ''
-    install -d $out/share/fonts/truetype/NerdFonts/JetBrainsMonoCondensed
-    cp $src/*.ttf $out/share/fonts/truetype/NerdFonts/JetBrainsMonoCondensed
+    runHook preInstall
+    install -Dm644 -t $out/share/fonts/truetype/NerdFonts/JetBrainsMonoCondensed *.ttf
+    runHook postInstall
   '';
 
   meta = with lib; {
